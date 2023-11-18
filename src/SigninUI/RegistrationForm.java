09757fcff8a8937be173f3974407a64d593875db
Chrimika <mbachristian58@gmail.com>
2023-11-18 12:47:51 +0100

    Interfaces de connexion des gestionnaire des chambres

---
+++ src/SigninUI/RegistrationForm.java
@@ -0,0 +1,155 @@
+package SigninUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import Login.User;
+import LoginUI.LoginForm;
+
+public class RegistrationForm extends JDialog{
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField tfAddress;
+    private JPasswordField tfPassword;
+    private JPasswordField tfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel registerPanel;
+    private JButton btnLogin;
+
+    public RegistrationForm(JFrame parent){
+        super(parent);
+        setTitle("Create a new account");
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension(550, 550));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                LoginForm loginForm = new LoginForm(null);
+                //loginForm.setVisible(true);
+                /*loginForm.pack();
+                loginForm.setLocationRelativeTo(null);*/
+                //dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void registerUser() {
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        String address = tfAddress.getText();
+        String password = String.valueOf(tfPassword.getText());
+        String confirmPassword = String.valueOf(tfConfirmPassword.getText());
+
+        if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()){
+            JOptionPane.showMessageDialog(this,
+                    "Please enter all feilds",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!password.equals(confirmPassword)){
+            JOptionPane.showMessageDialog(this,
+                    "Confirm password does not match",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        user = addUserToDatabase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        }else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to register new user",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
+    public User user;
+    private User addUserToDatabase(String name, String email, String phone, String address, String  password){
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/auth_db";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            //Connected to db successfuly...
+
+            Statement stat  = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password) VALUES (?,?,?,?,?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0){
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stat.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public static void main(String[] args) {
+        RegistrationForm myForm = new RegistrationForm(null);
+        User user = myForm.user;
+        if (user != null){
+            LoginForm loginForm = new LoginForm(null);
+            User user1 = loginForm.user;
+            if (user != null){
+                System.out.println("Successful Authentication of : " + user.name);
+                System.out.println("        Email : " + user1.email);
+                System.out.println("        Phone : " + user1.phone);
+                System.out.println("        Address : " + user1.address);
+            }
+            else{
+                System.out.println("Authentication canceled");
+            }
+            System.out.println("Successful registration of : " + user1.name);
+        }else{
+            System.out.println("Resgistration canceled");
+        }
+    }
+}
